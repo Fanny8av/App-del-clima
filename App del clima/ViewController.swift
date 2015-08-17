7b1680e3e1f48a6e674c6b15e7b79d64092eb814
Fanny Ochoa <fannynimini@gmail.com>
2015-08-17 17:34:08 -0500

    App del clima

---
+++ App del clima/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App del clima
+//
+//  Created by Fanny Ochoa on 17/08/15.
+//  Copyright (c) 2015 Fanny Ochoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: <#UIImage#>(named: "iphone-4s-wallpaper-9.jpg")!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
